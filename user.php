04e206cf3c5fbe681c2ebee9808899f1d7acadf0
Noor <445202144@student.ksu.edu.sa>
2026-04-20 20:38:56 +0300

    Update user.php

--- user.php
+++ user.php
@@ -2,7 +2,7 @@
 session_start();
 include "db.php";
 if (!isset($_SESSION['user_id']) || $_SESSION['user_type'] != 'user') {
-    header("Location: login.php");
+   header("Location: login.php?error=unauthorized");
     exit();
 }
 
